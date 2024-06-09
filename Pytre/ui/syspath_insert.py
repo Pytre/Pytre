75c6925beee3afd156cbe4dc9c751361181e991b
Matth78 <matth.ferrier@gmail.com>
2024-06-09 17:56:16 +0200

    fix : insert syspath ajustement

--- Pytre/ui/syspath_insert.py
+++ Pytre/ui/syspath_insert.py
@@ -3,4 +3,5 @@ from pathlib import Path
 
 """add parent folder to sys path to allow scripts direct run"""
 main_dir = str(Path(__file__).parents[1])
-sys.path.insert(0, main_dir)
+if main_dir not in sys.path:
+    sys.path.insert(0, main_dir)

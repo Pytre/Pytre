9643ed9b4b73449b991914c5a822dcb27efdf3fe
Matth78 <Matth.Ferrier@gmail.com>
2025-07-01 12:02:38 +0200

    Version 1.5.3

--- Pytre/about.py
+++ Pytre/about.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from settings import get_app_path
 
 APP_NAME = "Pytre"
-APP_VERSION = "1.5.2"
+APP_VERSION = "1.5.3"
 APP_STATUS = ""
 APP_BUILD = "1"
 COPYRIGHT_YEAR = "2021"
